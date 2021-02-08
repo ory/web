68672abd0b94d5658c21341f2bc933065a065252
jfcurran <john@ory.sh>
2021-02-08 13:31:03 +0100

    feat: create .next directory and add home.tsx
    - .next directory will serve as location for new pages until they are deployed
    - home.tsx is an empty page which will be filled with following commits

---
+++ src/pages/.next/home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../../components/layout'
+import SEO from '../../components/seo'
+
+
+const HomePage = () => (
+  <Layout>
+    <SEO
+      description=""
+      title=""
+    />
+
+  </Layout>
+)
+
+export default HomePage
