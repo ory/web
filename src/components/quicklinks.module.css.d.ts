a1047b51bb43f7eb8b55a089aa902b49560c0985
John <jfcurran@users.noreply.github.com>
2021-02-08 14:13:34 +0100

    feat: create quicklinks component (#112)
    * feat: create quicklinks component
    * fix: add new line to quicklinks.tsx after export

---
+++ src/components/quicklinks.module.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly "thin": string;
+  readonly "quicklinks": string;
+  readonly "quicklinksContent": string;
+  readonly "quicklinksBox": string;
+  readonly "quicklinksInner": string;
+  readonly "cap": string;
+};
+export = styles;
