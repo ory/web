7ded8e983dd79263de99ddec0b15e7cfa6c4f8a3
jfcurran <john@ory.sh>
2021-02-08 14:16:45 +0100

    feat: add new quicklinks.tsx to home.tsx
    -populate with placeholder content for testing

--- src/pages/.next/home.tsx
+++ src/pages/.next/home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../../components/layout'
 import SEO from '../../components/seo'
+import Quicklinks from '../../components/quicklinks'
 
 
 const HomePage = () => (
@@ -9,7 +10,45 @@ const HomePage = () => (
       description=""
       title=""
     />
-
+  <Quicklinks
+    title={"Placeholder"}
+    description={"lorem ipsum"}
+    cta={[
+      {
+        title: 'Test',
+        href:'/hydra',
+        style: 'primary',
+      },
+      {
+        title: 'GitHub',
+        href: '/kratos',
+        style: 'secondary'
+      }
+    ]}
+    quick={[
+      {
+        description: 'lorem ipsum',
+        learn: 'link',
+        href: '/hydra',
+        openInNewWindow: true,
+        icon: 'placeholder1',
+      },
+      {
+        description: 'lorem ipsum',
+        learn: 'link',
+        href: '/hydra',
+        openInNewWindow: true,
+        icon: 'placeholder2',
+      },
+      {
+        description: 'lorem ipsum',
+        learn: 'link',
+        href: '/hydra',
+        openInNewWindow: true,
+        icon: 'placeholder3',
+      },
+    ]}
+    />
   </Layout>
 )
 
