b3c70dc770033b0aa5c5f2034bf8d3cfaaa5347a
jfcurran <john@ory.sh>
2021-02-08 14:44:46 +0100

    feat: add new large-feature.tsx to home.tsx
    - populate with placeholder content for testing

--- src/pages/.next/home.tsx
+++ src/pages/.next/home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../../components/layout'
 import SEO from '../../components/seo'
 import Quicklinks from '../../components/quicklinks'
+import LargeFeature from '../../components/large-feature'
 
 
 const HomePage = () => (
@@ -10,44 +11,62 @@ const HomePage = () => (
       description=""
       title=""
     />
-  <Quicklinks
-    title={"Placeholder"}
-    description={"lorem ipsum"}
-    cta={[
-      {
-        title: 'Test',
-        href:'/hydra',
-        style: 'primary',
-      },
-      {
-        title: 'GitHub',
-        href: '/kratos',
-        style: 'secondary'
-      }
-    ]}
-    quick={[
-      {
-        description: 'lorem ipsum',
-        learn: 'link',
-        href: '/hydra',
-        openInNewWindow: true,
-        icon: 'placeholder1',
-      },
-      {
-        description: 'lorem ipsum',
-        learn: 'link',
-        href: '/hydra',
-        openInNewWindow: true,
-        icon: 'placeholder2',
-      },
-      {
-        description: 'lorem ipsum',
-        learn: 'link',
-        href: '/hydra',
-        openInNewWindow: true,
-        icon: 'placeholder3',
-      },
-    ]}
+
+    <LargeFeature
+      title={'Placeholder'}
+      description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non ipsum interdum, consequat metus in, lobortis nunc. Praesent tempus tellus vitae gravida tempus. Vestibulum malesuada'}
+      learn={'Dolor Sit'}
+      href={'/kratos'}
+      visual={'Placeholder1'}
+    />
+
+    <LargeFeature
+      alternate
+      title={'Placeholder'}
+      description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi non ipsum interdum, consequat metus in, lobortis nunc. Praesent tempus tellus vitae gravida tempus. Vestibulum malesuada'}
+      learn={'Dolor Sit'}
+      href={'/kratos'}
+      visual={'Placeholder2'}
+    />
+
+    <Quicklinks
+      title={'Placeholder'}
+      description={'lorem ipsum'}
+      cta={[
+        {
+          title: 'Test',
+          href: '/hydra',
+          style: 'primary',
+        },
+        {
+          title: 'GitHub',
+          href: '/kratos',
+          style: 'secondary',
+        },
+      ]}
+      quick={[
+        {
+          description: 'lorem ipsum',
+          learn: 'link',
+          href: '/hydra',
+          openInNewWindow: true,
+          icon: 'placeholder1',
+        },
+        {
+          description: 'lorem ipsum',
+          learn: 'link',
+          href: '/hydra',
+          openInNewWindow: true,
+          icon: 'placeholder2',
+        },
+        {
+          description: 'lorem ipsum',
+          learn: 'link',
+          href: '/hydra',
+          openInNewWindow: true,
+          icon: 'placeholder3',
+        },
+      ]}
     />
   </Layout>
 )
