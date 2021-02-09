198afa76c94c6eb0bd1fbce0e6f0136f9df905b1
John <jfcurran@users.noreply.github.com>
2021-02-09 10:51:33 +0100

    feat: edit highlights.tsx to include icons and appearance (#119)
    - fix css responsiveness
    - set proper margins

--- src/components/highlights.tsx
+++ src/components/highlights.tsx
@@ -3,11 +3,17 @@ import { Link } from 'gatsby'
 import * as styles from './highlights.module.css'
 import cn from 'classnames'
 
+import Placeholder1 from '../images/icon/GitHub.svg'
+import Placeholder2 from '../images/icon/twitter.svg'
+import Placeholder3 from '../images/icon/linkedin.svg'
+import Integration from '../images/illustrations/integration.svg'
+
 interface PropTypes {
   highlight: Highlight[]
 }
 
 interface Highlight {
+  visual: 'hydra' | 'keto' | 'kratos' | 'oathkeeper'
   title: string
   content: string
   href: string
@@ -24,19 +30,35 @@ const Highlights = ({ highlight }: PropTypes) => (
             styles.highlightRow
           )}
         >
-          {highlight.map(({ title, content, href, openInNewWindow }) => (
-            <Link
-              key={title}
-              to={href}
-              rel={openInNewWindow ? 'noopener noreferrer' : ''}
-              target={openInNewWindow ? '_blank' : ''}
-              className={cn(styles.highlightBox)}>
-              <div className={cn('col-lg-offset-1 col-lg-10')}>
-                <h3>{title}</h3>
-                <p>{content}</p>
-              </div>
-            </Link>
-          ))}
+          {highlight.map(
+            ({ visual, title, content, href, openInNewWindow }) => (
+              <Link
+                key={title}
+                to={href}
+                rel={openInNewWindow ? 'noopener noreferrer' : ''}
+                target={openInNewWindow ? '_blank' : ''}
+                className={cn(styles.highlightBox)}
+              >
+                <div className={cn('col-lg-offset-1 col-lg-10')}>
+                  <img
+                    src={
+                      visual === 'hydra'
+                        ? Placeholder1
+                        : visual === 'keto'
+                        ? Placeholder2
+                        : visual === 'kratos'
+                        ? Placeholder3
+                        : visual === 'oathkeeper'
+                        ? Integration
+                        : ''
+                    }
+                  />
+                  <h3>{title}</h3>
+                  <p className={'secondary'}>{content}</p>
+                </div>
+              </Link>
+            )
+          )}
         </div>
       </div>
     </div>
