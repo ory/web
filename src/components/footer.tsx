e51d16032af76c298d087e70f1bd8c52d84f0636
jfcurran <john@ory.sh>
2021-02-09 19:30:57 +0100

    feat: change appearance and structure of footer.tsx
    - footer now has more responsiveness
    - added type to const menu `MenuItem`
    - list-titles are now clickable links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,6 +4,7 @@ import logo from '../images/logo-ory-footer.svg'
 import logoTiny from '../images/logo-ory-footer-tiny.svg'
 import styles from './footer.module.css'
 import { brandPrefix } from '../config'
+import { Link } from 'gatsby'
 
 const comp = [
   {
@@ -24,9 +25,58 @@ const comp = [
   }
 ]
 
-const menu = [
+interface MenuItem {
+  openInNewWindow?: boolean
+  title: string
+  href: string
+  items: {
+    openInNewWindow?: boolean
+    title: string
+    href: string
+  }[]
+}
+
+const menu: MenuItem[] = [
+  {
+    title: 'Product',
+    href: '/foo',
+    items: [
+      {
+        title: 'Authentication',
+        href: '/hydra',
+        openInNewWindow: true
+      },
+      {
+        title: 'Authorization',
+        href: '/keto'
+      },
+      {
+        title: 'User Management',
+        href: '/kratos'
+      },
+      {
+        title: 'Zero Trust',
+        href: '/oathkeeper'
+      }
+    ]
+  },
+  {
+    title: 'Developer',
+    href: '/foo',
+    items: [
+      {
+        title: 'Documentation',
+        href: '/docs'
+      },
+      {
+        title: 'SDKs',
+        href: 'https://github.com/ory/sdk'
+      }
+    ]
+  },
   {
     title: 'Resources',
+    href: '/foo',
     items: [
       {
         title: 'Docs',
@@ -43,23 +93,29 @@ const menu = [
     ]
   },
   {
-    title: 'GitHub',
+    title: 'Open Source',
+    href: '/opensource',
+    openInNewWindow: true,
     items: [
       {
         title: `${brandPrefix}Kratos`,
-        href: 'https://github.com/ory/kratos'
+        href: 'https://github.com/ory/kratos',
+        openInNewWindow: true
       },
       {
         title: `${brandPrefix}Hydra`,
-        href: 'https://github.com/ory/hydra'
+        href: 'https://github.com/ory/hydra',
+        openInNewWindow: true
       },
       {
         title: `${brandPrefix}Oathkeeper`,
-        href: 'https://github.com/ory/oathkeeper'
+        href: 'https://github.com/ory/oathkeeper',
+        openInNewWindow: true
       },
       {
         title: `${brandPrefix}Keto`,
-        href: 'https://github.com/ory/keto'
+        href: 'https://github.com/ory/keto',
+        openInNewWindow: true
       }
     ]
   }
@@ -69,31 +125,52 @@ const Footer = () => (
   <footer className={cn(styles.footer)}>
     <div className="container-fluid">
       <div className="row">
-        <div className="col-lg-offset-1 col-lg-2 col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10 center-mobile">
+        <div
+          className={cn(
+            'col-lg-offset-1 col-lg-10 col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10',
+            styles.divider
+          )}
+        />
+      </div>
+
+      <div className="row">
+        <div className="col-lg-offset-1 col-lg-2 col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10">
           {brandPrefix.length > 0 ? (
             <>
-              <img loading="lazy" src={logo} alt={'ORY logo'} />
+              <img loading="lazy" src={logo} alt={'logo'} />
               <p className={cn(styles.contact)}>
-                Get in touch with us
-                <br />
                 <a href="mailto:office@ory.sh">office@ory.sh</a>
+                <br />
+                👋 Say Hello
               </p>
             </>
           ) : (
             <>
-              <img loading="lazy" src={logoTiny} alt={'ORY logo'} />
+              <img loading="lazy" src={logoTiny} alt={'Ory logo'} />
             </>
           )}
         </div>
-        <div className="col-lg-7 col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10 center-mobile mobile-offset-64">
+        <div className="col-lg-offset-1 col-lg-7 col-md-offset-1 col-md-10 col-sm-offset-1 col-md-offset-1 col-sm-10">
           <div className={cn('row', styles.menuRow)}>
-            {menu.map(({ title, items }) => (
+            {menu.map(({ title, items, href, openInNewWindow = false }) => (
               <div key={title} className={styles.menuItem}>
-                <p className={styles.listTitle}>{title}</p>
+                <Link
+                  to={href}
+                  rel={openInNewWindow ? 'noopener noreferrer' : ''}
+                  target={openInNewWindow ? '_blank' : ''}
+                >
+                  <p className={styles.listTitle}>{title}</p>
+                </Link>
                 <ul className={styles.list}>
-                  {items.map(({ title, href }) => (
+                  {items.map(({ title, href, openInNewWindow = false }) => (
                     <li key={title} className={styles.item}>
-                      <a href={href}>{title}</a>
+                      <Link
+                        to={href}
+                        rel={openInNewWindow ? 'noopener noreferrer' : ''}
+                        target={openInNewWindow ? '_blank' : ''}
+                      >
+                        {title}
+                      </Link>
                     </li>
                   ))}
                 </ul>
@@ -106,18 +183,18 @@ const Footer = () => (
       <div className="row">
         <div
           className={cn(
-            'col-lg-offset-1 col-lg-10 col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10 center-mobile',
+            'col-lg-offset-1 col-lg-10 col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10',
             styles.divider
           )}
         />
       </div>
 
       <div className="row">
-        <div className="col-lg-offset-1 col-lg-3 col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10 center-mobile">
-          <p className={styles.copyright}>&copy; Copyright 2020 Ory Corp</p>
+        <div className="col-lg-offset-1 col-lg-3 col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10">
+          <p className={styles.copyright}>&copy; Copyright 2021 Ory Corp</p>
         </div>
 
-        <div className="col-lg-6 col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10 center-mobile">
+        <div className="col-lg-6 col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10">
           <div className={cn('row', styles.compRow)}>
             {comp.map(({ items }, index) => (
               <div key={index} className={styles.menuItem}>
