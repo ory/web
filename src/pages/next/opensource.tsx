52a669639025f82745b4d4f164e612fe485ff325
vinckr <vincent@ory.sh>
2021-05-17 14:21:01 +0200

    chore: format

--- src/pages/next/opensource.tsx
+++ src/pages/next/opensource.tsx
@@ -56,14 +56,16 @@ const IndexPage = () => (
       image={<img loading="lazy" alt="" src={opensource} />}
     />
 
-  <Quickstart
+    <Quickstart
       title={<>Ready to get started?</>}
       description={
         <>
-          Explore Ory and the future of identity. 
-          <br />Make yourself heard in discussions and contribute directly to Ory on GitHub. 
-          <br />Your code will be shipped in the next version.
-
+          Explore Ory and the future of identity.
+          <br />
+          Make yourself heard in discussions and contribute directly to Ory on
+          GitHub.
+          <br />
+          Your code will be shipped in the next version.
         </>
       }
       buttons={
@@ -80,10 +82,17 @@ const IndexPage = () => (
       content={[
         {
           icon: <BookOpen size={32} />,
-          title: <>Slack:<br />Developer Chat</>,
+          title: (
+            <>
+              Slack:
+              <br />
+              Developer Chat
+            </>
+          ),
           description: (
             <>
-              A community to help you succeed. Become a hero for developers, ask questions & participate in events.
+              A community to help you succeed. Become a hero for developers, ask
+              questions & participate in events.
             </>
           ),
           button: (
@@ -98,11 +107,16 @@ const IndexPage = () => (
         },
         {
           icon: <Users size={32} />,
-          title: <>GitHub:<br /> Discussions & Code</>,
+          title: (
+            <>
+              GitHub:
+              <br /> Discussions & Code
+            </>
+          ),
           description: (
             <>
-              Make yourself heard on GitHub and contribute code and insights to the open source projects at
-              the heart of Ory.
+              Make yourself heard on GitHub and contribute code and insights to
+              the open source projects at the heart of Ory.
             </>
           ),
           button: (
@@ -118,25 +132,27 @@ const IndexPage = () => (
       ]}
     />
     <Stats />
-    
+
     <FeatureImage
       overline={'User & Identity Management'}
       title={<>Ory Kratos</>}
       description={
         <>
-          Grow your users, easily register and manage user identities, using custom flows in your look and feel.
+          Grow your users, easily register and manage user identities, using
+          custom flows in your look and feel.
         </>
-      }      
+      }
       buttons={
         <>
-          <Button to={'docs/kratos/next/'} 
-          style={'link'}
-          iconRight={<ArrowRight size={16} />}
+          <Button
+            to={'docs/kratos/next/'}
+            style={'link'}
+            iconRight={<ArrowRight size={16} />}
           >
-          Install Kratos
+            Install Kratos
           </Button>
         </>
-      }    
+      }
       image={<img loading="lazy" alt="" src={kratos} />}
     />
 
@@ -146,67 +162,70 @@ const IndexPage = () => (
       title={<>Ory Hydra</>}
       description={
         <>
-          Keep threats out and easily verify who is a user and customize sign in flows that fit you and your users.
+          Keep threats out and easily verify who is a user and customize sign in
+          flows that fit you and your users.
         </>
       }
       buttons={
         <>
-          <Button to={'docs/hydra/next/'} 
-          style={'link'}
-          iconRight={<ArrowRight size={16} />}
+          <Button
+            to={'docs/hydra/next/'}
+            style={'link'}
+            iconRight={<ArrowRight size={16} />}
           >
-          Install Hydra
+            Install Hydra
           </Button>
         </>
       }
       image={<img loading="lazy" alt="" src={hydra} />}
-    />    
-    
+    />
+
     <FeatureImage
-    overline={'Authorization'}
-    title={<>Ory Keto</>}
-    description={
-      <>
-        Keep threats out and easily verify who is a user and customize sign in flows that fit you and your users.
-      </>
-    }  
-    buttons={
-      <>
-        <Button to={'docs/keto/next/'}      
-        style={'link'}
-        iconRight={<ArrowRight size={16} />}
-        >
-        Install Keto
-        </Button>
-      </>
-    }
-    image={<img loading="lazy" alt="" src={keto} />}
-  />    
-  
-  <FeatureImage
-  mirror={true}
-  overline={'Zero Trust Networking'}
-  title={<>Ory Oathkeeper</>}
-  description={
-    <>
-      Be in full control - secure webfacing applications and services by implementing Zero Trust Network Architecture.
-    </>
-  }      
-  buttons={
-    <>
-      <Button to={'docs/oathkeeper/next/'} 
-      style={'link'}
-      iconRight={<ArrowRight size={16} />}
-      >
-      
+      overline={'Authorization'}
+      title={<>Ory Keto</>}
+      description={
+        <>
+          Keep threats out and easily verify who is a user and customize sign in
+          flows that fit you and your users.
+        </>
+      }
+      buttons={
+        <>
+          <Button
+            to={'docs/keto/next/'}
+            style={'link'}
+            iconRight={<ArrowRight size={16} />}
+          >
+            Install Keto
+          </Button>
+        </>
+      }
+      image={<img loading="lazy" alt="" src={keto} />}
+    />
 
-      Install Oathkeeper
-      </Button>
-    </>
-  }
-  image={<img loading="lazy" alt="" src={oathkeeper} />}
-/>
-
+    <FeatureImage
+      mirror={true}
+      overline={'Zero Trust Networking'}
+      title={<>Ory Oathkeeper</>}
+      description={
+        <>
+          Be in full control - secure webfacing applications and services by
+          implementing Zero Trust Network Architecture.
+        </>
+      }
+      buttons={
+        <>
+          <Button
+            to={'docs/oathkeeper/next/'}
+            style={'link'}
+            iconRight={<ArrowRight size={16} />}
+          >
+            Install Oathkeeper
+          </Button>
+        </>
+      }
+      image={<img loading="lazy" alt="" src={oathkeeper} />}
+    />
 
     <Quotes
       title={
@@ -224,27 +243,42 @@ const IndexPage = () => (
           ),
           description: (
             <>
-              We like using Ory because it’s API first design made it super easy to integrate with the rest our stack.
+              We like using Ory because it’s API first design made it super easy
+              to integrate with the rest our stack.
             </>
           ),
           person: `Paul Harman`,
           jobTitle: `Engineering Manager, Sainsbury’s Tech`
         },
         {
-          logo: <img src={thoughtworks} loading="lazy" alt={'Thoughtworks Logo'} />,
-          description: <>Securing modern digital systems requires new approaches to identity. With Ory, I can build custom and secure ID infrastructure and APIs that fit perfectly into my client's ever-evolving cloud environments.</>,
+          logo: (
+            <img src={thoughtworks} loading="lazy" alt={'Thoughtworks Logo'} />
+          ),
+          description: (
+            <>
+              Securing modern digital systems requires new approaches to
+              identity. With Ory, I can build custom and secure ID
+              infrastructure and APIs that fit perfectly into my client's
+              ever-evolving cloud environments.
+            </>
+          ),
           person: `Ken Adler`,
           jobTitle: `Identity & Infosec, Thoughtworks`
-        },        
+        },
         {
           logo: <img src={blues} loading="lazy" alt={'Blues Logo'} />,
-          description: <>I needed an Auth System that would work with modern setups, and I did not want to write it myself. Ory fit the bill perfectly and just works.</>,
+          description: (
+            <>
+              I needed an Auth System that would work with modern setups, and I
+              did not want to write it myself. Ory fit the bill perfectly and
+              just works.
+            </>
+          ),
           person: `Steff Kelsey`,
           jobTitle: `Head of Engineering. Blues Wireless`
         }
       ]}
     />
-
   </Layout>
 )
 
