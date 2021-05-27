c21f2038213168a36e073e1e022efe16b04a2164
jfcurran <john@ory.sh>
2021-05-27 19:22:36 +0200

    coding.tsx - clean up import - style code.tsx using font classes from typography.tsx
    Signed-off-by: jfcurran <john@ory.sh>

--- src/components/layouts/coding/coding.tsx
+++ src/components/layouts/coding/coding.tsx
@@ -7,17 +7,12 @@ import ContentText from '../../freestanding/content/content-text'
 import MoleculeTextInteraction from '../../freestanding/molecule/molecule-text-interaction'
 import Molecule from '../../freestanding/molecule/molecule'
 import MoleculeInteraction from '../../freestanding/molecule/molecule-interaction'
-import Button from '../../freestanding/button/button'
-import { ArrowRight, ArrowSquareIn, GitDiff } from 'phosphor-react'
-import Terminal from '../../freestanding/code/terminal'
-import Code from '../../freestanding/code/code'
 import {
   pb16,
   pb32,
   pb64,
+  pb8,
   pt24,
-  pt48,
-  pt64,
   pt8
 } from '../../freestanding/utils/padding.module.css'
 import ColourWrapper from '../../freestanding/colour/colour-wrapper'
@@ -51,23 +46,25 @@ const Coding = ({
     <Container fluid={true}>
       <Grid lg={6} md={6} sm={12} xs={12}>
         <Container flexContainer={'column'}>
-          <ContentText>
-            <h3 className={cn('font-overline-light', pb16)}>{overline}</h3>
-            <h2 className={cn('font-h3-light', pb32)}>{title}</h2>
-            <p className={cn('font-p-light')}>{description}</p>
+          <ContentText className={cn(pb64)}>
+            <h3 className={cn('font-overline', 'light', pb16)}>{overline}</h3>
+            <h2 className={cn('font-h3', 'light', pb32)}>{title}</h2>
+            <p className={cn('font-p', 'light', 'mute-85', pb32)}>
+              {description}
+            </p>
             {buttons && (
-              <MoleculeInteraction className={cn(pt48)}>
+              <MoleculeInteraction className={''}>
                 {buttons}
               </MoleculeInteraction>
             )}
           </ContentText>
 
-          <Container alignItems={'start'} className={cn(pt64)}>
+          <Container alignItems={'start'}>
             {content.map(({ button, description, icon, title }, index) => (
               <Grid
                 lg={6}
                 md={6}
-                sm={12}
+                sm={6}
                 xs={12}
                 className={cn(pb64)}
                 key={index}
@@ -75,17 +72,15 @@ const Coding = ({
                 <ContentText>
                   <MoleculeTextInteraction>
                     <Molecule>
-                      <ColourWrapper text={'themed-darkmode'}>
+                      <ColourWrapper text={'themed-light'} className={cn(pb8)}>
                         {icon}
                       </ColourWrapper>
-                      <h4 className={cn('font-h5-light', pt8)}>{title}</h4>
-                      <p className={cn('font-p-sm-light', pt8)}>
+                      <h4 className={cn('font-h5', 'light', pb8)}>{title}</h4>
+                      <p className={cn('font-p-sm', 'mute-85', 'light', pb8)}>
                         {description}
                       </p>
                     </Molecule>
-                    <MoleculeInteraction className={cn(pt24)}>
-                      {button}
-                    </MoleculeInteraction>
+                    <MoleculeInteraction>{button}</MoleculeInteraction>
                   </MoleculeTextInteraction>
                 </ContentText>
               </Grid>
