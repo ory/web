d3184ff024b62b7b83898808c922b51fa31f9269
jfcurran <john@ory.sh>
2021-05-27 17:02:10 +0200

    features - clean up imports - improve flexing on all viewports using grid component - style using classes from typography.css
    Signed-off-by: jfcurran <john@ory.sh>

--- src/components/layouts/features/features.tsx
+++ src/components/layouts/features/features.tsx
@@ -7,14 +7,9 @@ import ContentText from '../../freestanding/content/content-text'
 import Molecule from '../../freestanding/molecule/molecule'
 import {
   pb16,
-  pb24,
   pb48,
   pb64,
   pb8,
-  pl32,
-  pr16,
-  pr32,
-  pr8,
   pt24,
   pt32
 } from '../../freestanding/utils/padding.module.css'
@@ -44,10 +39,10 @@ const Features = ({
 }: PropTypes) => (
   <div className={cn(features)}>
     <Container fluid={true}>
-      <Grid lg={6} md={6} sm={12} xs={12} className={cn(pb64)}>
+      <Grid lg={4} md={4} sm={12} xs={12} className={cn(pb64)}>
         <ContentText>
           <Molecule>
-            <h3 className={cn('font-overline', pb16)}>{overline}</h3>
+            <h3 className={cn('font-overline', 'primary', pb16)}>{overline}</h3>
             <h2 className={cn('font-h3')}>{title}</h2>
             {description && <p className={cn('font-p', pt32)}>{description}</p>}
           </Molecule>
@@ -61,29 +56,23 @@ const Features = ({
           {feature.map((f, index) => {
             return (
               <Grid
-                lg={5}
-                md={5}
-                sm={12}
+                lg={6}
+                md={6}
+                sm={6}
                 xs={12}
-                className={cn(pb64, featuresContent)}
+                className={cn(pb48, featuresContent)}
                 key={index}
               >
-                <Container
-                  flexContainer={'row'}
-                  alignItems={'start'}
-                  justify={'space-between'}
-                >
-                  <Grid lg={3} md={3} sm={1} xs={2}>
-                    <ColourWrapper text={'themed-primary'}>
-                      {f.icon}
-                    </ColourWrapper>
-                  </Grid>
-                  <Grid lg={9} md={9} sm={10} xs={10}>
-                    <ContentText>
-                      <h4 className={cn('font-h5', pb8)}>{f.title}</h4>
-                      <p className={cn('font-p-sm')}>{f.description}</p>
-                    </ContentText>
-                  </Grid>
+                <Container flexContainer={'row'} alignItems={'start'}>
+                  <ColourWrapper className={cn(pb8)} text={'themed-primary'}>
+                    {f.icon}
+                  </ColourWrapper>
+                  <ContentText>
+                    <h4 className={cn('font-h5', pb8)}>{f.title}</h4>
+                    <p className={cn('font-p-sm', 'mute-60', pb8)}>
+                      {f.description}
+                    </p>
+                  </ContentText>
                 </Container>
               </Grid>
             )
