691c526a0256591f36918087d8a7597d1ad0e571
jfcurran <john@ory.sh>
2021-05-27 16:58:36 +0200

    quickstart - clean up imports - style content using classes from typography.css - improve flexbox behavior using grid component and padding util
    Signed-off-by: jfcurran <john@ory.sh>

--- src/components/layouts/quickstart/quickstart.tsx
+++ src/components/layouts/quickstart/quickstart.tsx
@@ -4,7 +4,6 @@ import cn from 'classnames'
 import ContentText from '../../freestanding/content/content-text'
 import Molecule from '../../freestanding/molecule/molecule'
 import MoleculeInteraction from '../../freestanding/molecule/molecule-interaction'
-import Button from '../../freestanding/button/button'
 import Container from '../../freestanding/containers/container'
 import Grid from '../../freestanding/containers/grid'
 import ColourWrapper from '../../freestanding/colour/colour-wrapper'
@@ -12,8 +11,7 @@ import {
   pb32,
   pb48,
   pb64,
-  pb8,
-  pt24
+  pb8
 } from '../../freestanding/utils/padding.module.css'
 
 export interface QuickstartContent {
@@ -32,12 +30,12 @@ interface PropType {
 
 const Quickstart = ({ title, description, buttons, content }: PropType) => (
   <div className={cn(styles.quickstart)}>
-    <Container fluid={true} alignItems={'start'}>
-      <Grid lg={6} md={6} sm={12} xs={12} className={cn(pb64)}>
+    <Container fluid={true} alignItems={'center'}>
+      <Grid lg={4} md={4} sm={12} xs={12} className={cn(pb64)}>
         <ContentText>
           <Molecule>
             <h3 className={cn('font-h3', pb32)}>{title}</h3>
-            <p className={cn('font-p', pb48)}>{description}</p>
+            <p className={cn('font-p', 'mute-60', pb32)}>{description}</p>
           </Molecule>
           <MoleculeInteraction>{buttons}</MoleculeInteraction>
         </ContentText>
@@ -46,26 +44,24 @@ const Quickstart = ({ title, description, buttons, content }: PropType) => (
         <Container alignItems={'start'}>
           {content.map(({ button, icon, description, title }, index) => (
             <Grid
-              lg={5}
-              md={5}
-              sm={12}
+              lg={6}
+              md={6}
+              sm={6}
               xs={12}
               className={cn(pb48, styles.quickstartContent)}
               key={index}
             >
               <Container flexContainer={'row'} alignItems={'start'}>
-                <Grid lg={3} md={3} sm={1} xs={2}>
-                  <ColourWrapper text={'themed-primary'}>{icon}</ColourWrapper>
-                </Grid>
-                <Grid lg={9} md={9} sm={10} xs={10}>
-                  <ContentText>
-                    <h4 className={cn('font-h5', pb8)}>{title}</h4>
-                    <p className={cn('font-p-sm')}>{description}</p>
-                    <MoleculeInteraction className={pt24}>
-                      {button}
-                    </MoleculeInteraction>
-                  </ContentText>
-                </Grid>
+                <ColourWrapper className={cn(pb8)} text={'themed-primary'}>
+                  {icon}
+                </ColourWrapper>
+                <ContentText>
+                  <h4 className={cn('font-h5', pb8)}>{title}</h4>
+                  <p className={cn('font-p-sm', 'mute-60', pb8)}>
+                    {description}
+                  </p>
+                  <MoleculeInteraction>{button}</MoleculeInteraction>
+                </ContentText>
               </Container>
             </Grid>
           ))}
