0d77a2e44faefeb1478b7e908ee21a1b336ccefb
jfcurran <john@ory.sh>
2021-05-31 11:30:03 +0200

    change footer responsiveness, add typography styles
    Signed-off-by: jfcurran <john@ory.sh>

--- src/components/layouts/footer/footer.module.css.d.ts
+++ src/components/layouts/footer/footer.module.css.d.ts
@@ -1,6 +1,7 @@
 declare const styles: {
   readonly footer: string
   readonly footerContainer: string
+  readonly footerCategories: string
   readonly footerImg: string
   readonly footerFont: string
 }
