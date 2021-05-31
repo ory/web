4ebde2a246311eba0922e44b1835c99328eebcbb
jfcurran <john@ory.sh>
2021-05-31 22:40:07 +0200

    clean up imports, pass adoptersDefault props
    Signed-off-by: jfcurran <john@ory.sh>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,16 +26,9 @@ import heroIllustration from '../images/illustrations/hero.svg'
 import headlessIllustration from '../images/illustrations/examplelogin.svg'
 import dashboardIllustration from '../images/illustrations/dashboard.svg'
 import codeboxIllustration from '../images/illustrations/codebox.svg'
-import blues from '../images/adopters/blues.svg'
-import thoughtworks from '../images/adopters/thoughtworks.svg'
-import kyma from '../images/adopters/kyma.svg'
-import raspberrypi from '../images/adopters/raspberrypi.svg'
-import segment from '../images/adopters/segment.svg'
-import sainsburys from '../images/adopters/sainsburys.svg'
-import hootsuite from '../images/adopters/hootsuite.svg'
-import threerein from '../images/adopters/threerein.svg'
 
 import SEO from '../components/layouts/seo/seo'
+import { adoptersDefault } from '../components/layouts/adopters/adopters-content'
 
 const IndexPage = () => (
   <Layout>
@@ -76,58 +69,7 @@ const IndexPage = () => (
       }
     />
 
-    <Adopters
-      adopters={[
-        {
-          title: 'BluesWireless',
-          image: blues,
-          url: 'https://blues.io/',
-          pos: 1
-        },
-        {
-          title: 'ThoughtWorks',
-          image: thoughtworks,
-          url: 'https://www.thoughtworks.com',
-          pos: 8
-        },
-        {
-          title: 'Segment',
-          image: segment,
-          url: 'https://segment.com/',
-          pos: 5
-        },
-        {
-          title: 'Raspberry Pi',
-          image: raspberrypi,
-          url: 'https://www.raspberrypi.org/',
-          pos: 3
-        },
-        {
-          title: `Sainsbury's`,
-          image: sainsburys,
-          url: 'https://www.sainsburys.co.uk/',
-          pos: 2
-        },
-        {
-          title: 'Hootsuite',
-          image: hootsuite,
-          url: 'https://hootsuite.com',
-          pos: 7
-        },
-        {
-          title: 'Kyma Project',
-          image: kyma,
-          url: 'https://kyma-project.io/',
-          pos: 4
-        },
-        {
-          title: '3REIN',
-          image: threerein,
-          url: 'https://3rein.com/',
-          pos: 6
-        }
-      ]}
-    />
+    <Adopters {...adoptersDefault} />
 
     <FeatureImage
       mirror={true}
