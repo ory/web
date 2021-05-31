e2153c10bd321318280476886ce7a9a1bb78ea9d
jfcurran <john@ory.sh>
2021-05-31 16:49:25 +0200

    add project.tsx to open-source page, add stats, clean up imports
    Signed-off-by: jfcurran <john@ory.sh>

--- src/pages/open-source.tsx
+++ src/pages/open-source.tsx
@@ -1,23 +1,4 @@
-import {
-  ArrowRight,
-  BookOpen,
-  CirclesThreePlus,
-  Code,
-  GitMerge,
-  LockOpen,
-  RocketLaunch,
-  Terminal,
-  UserPlus,
-  Users
-} from 'phosphor-react'
 import React from 'react'
-import Button from '../components/freestanding/button/button'
-import ColourWrapper from '../components/freestanding/colour/colour-wrapper'
-import Molecule from '../components/freestanding/molecule/molecule'
-import Adopters from '../components/layouts/adopters/adopters'
-import Coding from '../components/layouts/coding/coding'
-import FeatureImage from '../components/layouts/feature-image/feature-image'
-import Footer from '../components/layouts/footer/footer'
 import Hero from '../components/layouts/hero/hero'
 import Layout from '../components/layouts/layout/layout'
 import Quickstart from '../components/layouts/quickstart/quickstart'
@@ -25,21 +6,9 @@ import { quickstartOpenSource } from '../components/layouts/quickstart/quickstar
 import Stats from '../components/layouts/stats/stats'
 import Quotes from '../components/layouts/quotes/quotes'
 import opensource from '../images/illustrations/opensource.svg'
-import kratos from '../images/kratos/kratos.svg'
-import hydra from '../images/hydra/hydra.svg'
-import keto from '../images/keto/keto.svg'
-import oathkeeper from '../images/oathkeeper/oathkeeper.svg'
-import blues from '../images/adopters/blues.svg'
-import thoughtworks from '../images/adopters/thoughtworks.svg'
-import kyma from '../images/adopters/kyma.svg'
-import raspberrypi from '../images/adopters/raspberrypi.svg'
-import segment from '../images/adopters/segment.svg'
-import sainsburys from '../images/adopters/sainsburys.svg'
-import hootsuite from '../images/adopters/hootsuite.svg'
-import threerein from '../images/adopters/threerein.svg'
-import { pl32 } from '../components/freestanding/utils/padding.module.css'
-import cn from 'classnames'
 import SEO from '../components/layouts/seo/seo'
+import Projects from '../components/layouts/projects/projects'
+import { quotesDefault } from '../components/layouts/quotes/quotes-content'
 
 const IndexPage = () => (
   <Layout>
@@ -58,149 +27,28 @@ const IndexPage = () => (
       image={<img loading="lazy" alt="" src={opensource} />}
     />
 
-    <Quickstart {...quickstartOpenSource} />
-
-    <FeatureImage
-      overline={'User & Identity Management'}
-      title={<>Ory Kratos</>}
+    <Projects
+      overline={'foo'}
+      title={<>list of projects ay</>}
       description={
         <>
           Grow your users, easily register and manage user identities, using
           custom flows in your look and feel.
         </>
       }
-      buttons={
-        <>
-          <Button
-            to={'/docs/kratos/install/'}
-            style={'link'}
-            iconRight={<ArrowRight size={16} />}
-          >
-            Install Kratos
-          </Button>
-        </>
-      }
-      image={<img loading="lazy" alt="" src={kratos} />}
     />
 
-    <FeatureImage
-      mirror={true}
-      overline={'OAuth 2.0 and OpenId Connect'}
-      title={<>Ory Hydra</>}
+    <Stats
+      title={<>Billions of Identities</>}
       description={
         <>
-          Keep threats out and easily verify who is a user and customize sign in
-          flows that fit you and your users.
+          Companies from all over the world rely on Ory for their identity
+          needs. Ory technology secures billions of identity requests.
         </>
       }
-      buttons={
-        <>
-          <Button
-            to={'/docs/hydra/install/'}
-            style={'link'}
-            iconRight={<ArrowRight size={16} />}
-          >
-            Install Hydra
-          </Button>
-        </>
-      }
-      image={<img loading="lazy" alt="" src={hydra} />}
     />
-
-    <FeatureImage
-      overline={'Authorization'}
-      title={<>Ory Keto</>}
-      description={
-        <>
-          Personalize experiences using customer specific access rules, define
-          roles, and give customers individual access to your service.
-        </>
-      }
-      buttons={
-        <>
-          <Button
-            to={'/docs/keto/install/'}
-            style={'link'}
-            iconRight={<ArrowRight size={16} />}
-          >
-            Install Keto
-          </Button>
-        </>
-      }
-      image={<img loading="lazy" alt="" src={keto} />}
-    />
-
-    <FeatureImage
-      mirror={true}
-      overline={'Zero Trust Networking'}
-      title={<>Ory Oathkeeper</>}
-      description={
-        <>
-          Be in full control - secure webfacing applications and services by
-          implementing Zero Trust Network Architecture.
-        </>
-      }
-      buttons={
-        <>
-          <Button
-            to={'/docs/oathkeeper/install/'}
-            style={'link'}
-            iconRight={<ArrowRight size={16} />}
-          >
-            Install Oathkeeper
-          </Button>
-        </>
-      }
-      image={<img loading="lazy" alt="" src={oathkeeper} />}
-    />
-
-    <Quotes
-      title={<>Read about our users experience</>}
-      quotes={[
-        {
-          logo: (
-            <ColourWrapper text={'kratos-primary'}>
-              <img src={sainsburys} loading="lazy" alt={"Sainsbury's Logo"} />
-            </ColourWrapper>
-          ),
-          description: (
-            <>
-              We like using Ory because it’s API first design made it super easy
-              to integrate with the rest our stack.
-            </>
-          ),
-          person: `Paul Harman`,
-          jobTitle: `Engineering Manager, Sainsbury’s Tech`
-        },
-        {
-          logo: (
-            <img src={thoughtworks} loading="lazy" alt={'Thoughtworks Logo'} />
-          ),
-          description: (
-            <>
-              Securing modern digital systems requires new approaches to
-              identity. With Ory, I can build custom and secure ID
-              infrastructure and APIs that fit perfectly into my client's
-              ever-evolving cloud environments.
-            </>
-          ),
-          person: `Ken Adler`,
-          jobTitle: `Identity & Infosec, Thoughtworks`
-        },
-        {
-          logo: <img src={blues} loading="lazy" alt={'Blues Logo'} />,
-          description: (
-            <>
-              I needed an Auth System that would work with modern setups, and I
-              did not want to write it myself. Ory fit the bill perfectly and
-              just works.
-            </>
-          ),
-          person: `Steff Kelsey`,
-          jobTitle: `Head of Engineering. Blues Wireless`
-        }
-      ]}
-    />
+    <Quotes {...quotesDefault} />
+    <Quickstart {...quickstartOpenSource} />
   </Layout>
 )
 
